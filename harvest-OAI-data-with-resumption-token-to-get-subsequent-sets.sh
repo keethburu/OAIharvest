b8c33cea3bee46c2f579d8c9b43669bd62830cd9
peter kukla <pkukla@library.ohio.gov>
2017-07-20 16:08:29 -0400

    a few minor changes

--- harvest-OAI-data-with-resumption-token-to-get-subsequent-sets.sh
+++ harvest-OAI-data-with-resumption-token-to-get-subsequent-sets.sh
@@ -6,7 +6,7 @@
 #### across requests.
 ####
 
-SERVER="http://cdm16062.contentdm.oclc.org/oai/oai.php"
+SERVER="http://cdm16998.contentdm.oclc.org/oai/oai.php"
 
 DCGSETTINGS='--header="From: support@oclc.org" --header="User-Agent: WorldCat Digital Collection Gateway from OCLC.org"'
 
@@ -41,9 +41,9 @@ cp harvest*.sh output-from-harvest
 ###############################################
 
 
-wget "$SERVER?verb=ListRecords&metadataPrefix=oai_dc" $DCGSETTINGS -O oai-output.$counter.txt -o wget-errors-tmp.txt
+wget "$SERVER?verb=ListRecords&metadataPrefix=oai_dc&set=p16998coll2" $DCGSETTINGS -O oai-output.$counter.txt -o wget-errors-tmp.txt
 
-myResumptionToken=`xmllint --xpath '//*[local-name()="resumptionToken"]/text()' oai-output.0.txt`
+myResumptionToken=`xmllint --xpath '//*[local-name()="resumptionToken"]/text()' 2>/dev/null oai-output.0.txt`
 
 cat wget-errors-tmp.txt >> wget-errors.txt
 rm wget-errors-tmp.txt
@@ -52,7 +52,7 @@ mv oai-output.$counter.txt output-from-harvest
 let counter=counter+1
 
 echo '========================================================================================'
-echo "My Resumption Token:  $myResumptionToken"
+echo "( 0 )  My Resumption Token:  $myResumptionToken"
 echo '========================================================================================'
 while [[ $myResumptionToken != "" ]]
 do
@@ -61,14 +61,14 @@ do
   rm wget-errors-tmp.txt
 
 
-  myResumptionToken=`xmllint --xpath '//*[local-name()="resumptionToken"]/text()' oai-output.$counter.xml`
+  myResumptionToken=`xmllint --xpath '//*[local-name()="resumptionToken"]/text()' 2>/dev/null oai-output.$counter.xml`
   mv oai-output.$counter.xml output-from-harvest
-  echo "My Resumption Token:  $myResumptionToken"
+  echo "( $counter )  My Resumption Token:  $myResumptionToken"
   let counter=counter+1
 
 
-  ##### The following IF loop will adjust the numering of the output files when they reach 10 or 100.
-  ##### This allows for better sorting of the files.
+  ##### The following IF loop will add leading ZERO characters to output filenames when
+  ##### they reach 10, 100, 1000, 10000.  This allows for better sorting of the files.
 
   if [ $counter -eq 10 -o $counter -eq 100 -o $counter -eq 1000 -o $counter -eq 10000 ]
   then
